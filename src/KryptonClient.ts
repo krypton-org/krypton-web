5ffd74ac034c6b1f29f340fd7703d49c2e085bd3
jrebecchi <johann.corcuff@gmail.com>
2020-04-12 18:16:50 +0200

    Adding missing queries

--- src/KryptonClient.ts
+++ src/KryptonClient.ts
@@ -4,17 +4,27 @@ import {
     LoginQuery,
     UpdateQuery,
     DeleteQuery,
-    RegisterQuery
+    RegisterQuery,
+    EmailAvailableQuery,
+    SendVerificationEmailQuery,
+    UserOneQuery,
+    UserByIdsQuery,
+    UserManyQuery,
+    UserCountQuery,
+    UserPaginationQuery,
+    PublicKeyQuery
 } from './Queries'
 
 export default class KryptonClient {
     private endpoint: string;
+    private expiryDate: Date;
     private token: string;
     private user: Object;
 
     constructor(endpoint: string) {
         this.endpoint = endpoint;
         this.token = '';
+        this.expiryDate = new Date();
         this.user = {};
     }
 
@@ -24,7 +34,7 @@ export default class KryptonClient {
 
     public getAuthorizationHeader = (): string => 'Bearer ' + this.token;
 
-    public refresh = async (): Promise<void> => {
+    public refreshToken = async (): Promise<void> => {
         await this.query(new RefreshQuery(), false, true);
     }
 
@@ -47,6 +57,62 @@ export default class KryptonClient {
         let data: { deleteMe: boolean } = await this.query(new DeleteQuery({ password }), true);
         return data.deleteMe;
     }
+    /////////////////
+
+    public recoverPassword = async (email: string): Promise<boolean> => {
+        let data: { register: boolean } = await this.query(new UpdateQuery({ email }), true);
+        return data.register;
+    }
+
+    public emailAvailable = async (email: string): Promise<boolean> => {
+        let data: { emailAvailable: boolean } = await this.query(new EmailAvailableQuery({ email }), false);
+        return data.emailAvailable;
+    }
+
+    public changePassword = async (password: string, previousPassword: string): Promise<boolean> => {
+        let data: { register: boolean } = await this.query(new UpdateQuery({ password, previousPassword }), true);
+        return data.register;
+    }
+
+    public sendVerificationEmail = async () => {
+        let data: { sendVerificationEmail: boolean } = await this.query(new SendVerificationEmailQuery(), true, false);
+        return data.sendVerificationEmail;
+    }
+
+    public fetchUserOne = async (filter: Object, requestedFields: [string]) => {
+        let data: { userOne: any } = await this.query(new UserOneQuery({ filter }, requestedFields), true, false);
+        return data.userOne;
+    }
+
+    public fetchUserByIds = async (ids: [string], requestedFields: [string]) => {
+        let data: { userByIds: any } = await this.query(new UserByIdsQuery({ ids }, requestedFields), true, false);
+        return data.userByIds;
+    }
+
+    public fetchUserMany = async (filter: Object, requestedFields: [string], limit?: number) => {
+        let data: { userMany: any } = await this.query(new UserManyQuery({ filter, limit }, requestedFields), true, false);
+        return data.userMany;
+    }
+
+    public fetchUserCount = async (filter?: Object): Promise<number> => {
+        const variables: any = {}
+        if (filter){
+            variables.filter = filter;
+        }
+        let data: { userCount: number } = await this.query(new UserCountQuery(variables), true, false);
+        return data.userCount;
+    }
+
+    public fetchUserWithPagination = async (filter: Object, requestedFields: [string], page: number, perPage: number) => {
+        let data: { userOne: any } = await this.query(new UserPaginationQuery({ filter, page, perPage }, requestedFields), true, false);
+        return data.userOne;
+    }
+
+
+    public publicKey = async (): Promise<string> => {
+        let data: { publicKey: string } = await this.query(new PublicKeyQuery(), true, false);
+        return data.publicKey;
+    }
 
     private query = async (q: Query, isAuthTokenRequired: boolean, isRefreshed = false): Promise<any> => {
         const headers: any = {
@@ -67,7 +133,7 @@ export default class KryptonClient {
             const errorType = res.errors[0].type;
             const message = res.errors[0].message;
             if (errorType === 'UnauthorizedError' && !isRefreshed) {
-                await this.refresh();
+                await this.refreshToken();
                 return await this.query(q, isAuthTokenRequired, true)
             } else {
                 throw new (<any>window)[errorType](message);
@@ -77,17 +143,18 @@ export default class KryptonClient {
         const data = res.data;
 
         if (data.login) {
-            this.setUserAndToken(data.login.token);
+            this.setState(data.login.token, new Date(data.login.expiryDate));
         } else if (data.refreshToken) {
-            this.setUserAndToken(data.refreshToken.token);
+            this.setState(data.refreshToken.token, new Date(data.login.expiryDate));
         } else if (data.updateMe) {
-            this.setUserAndToken(data.updateMe.token);
+            this.setState(data.updateMe.token, new Date(data.login.expiryDate));
         }
 
         return data;
     }
 
-    private setUserAndToken = (token: string): void => {
+    private setState = (token: string, expiryDate: Date): void => {
+        this.expiryDate = expiryDate;
         this.token = token;
         this.user = JSON.parse(window.atob(token.split('.')[1]));
     }
--- src/Queries.ts
+++ src/Queries.ts
@@ -1,24 +1,48 @@
 export abstract class Query {
-    private variables: Object;
+    protected variables: any;
 
-    constructor(variables = {}){
+    constructor(variables = {}) {
         this.variables = variables;
     }
 
     public toJsonString = (): string => {
-        return JSON.stringify({ query: this.getQuery(), variables: this.variables});
+        return JSON.stringify({ query: this.getQuery(), variables: this.variables });
     };
-    
-    protected abstract getQuery() : string;
+
+    protected abstract getQuery(): string;
 }
 
-export class RefreshQuery extends Query{
+export abstract class QueryWithRequestedFields extends Query {
+    private requestedFields: [String];
+
+    constructor(variables: Object, requestedFields: [string]) {
+        super(variables);
+        this.requestedFields = requestedFields;
+    }
+
+    protected getRequestedFieldsFragment = (): string => {
+        return `
+            fragment requestedFields on UserPublicInfo {
+                `+this.requestedFields.join(" ")+`
+            }
+        `
+    }
+
+    public toJsonString = (): string => {
+        return JSON.stringify({ 
+            query: this.getQuery() + ' ' + this.getRequestedFieldsFragment(), 
+            variables: this.variables 
+        });
+    };
+}
+
+export class RefreshQuery extends Query {
     protected getQuery = (): string => `
-        mutation { refreshToken { token } }
+        mutation { refreshToken { token, expiryDate } }
     `;
 }
 
-export class RegisterQuery extends Query{
+export class RegisterQuery extends Query {
     protected getQuery = (): string => `
         mutation register($fields: UserRegisterInput!) {
             register(fields: $fields)
@@ -26,30 +50,126 @@ export class RegisterQuery extends Query{
     `;
 }
 
-export class LoginQuery extends Query{
+export class LoginQuery extends Query {
     protected getQuery = (): string => `
         mutation login($email: String!, $password: String!) {
             login(email: $email, password: $password) {
                 token
+                expiryDate
             }
         }
     `;
 }
 
-export class UpdateQuery extends Query{
+export class UpdateQuery extends Query {
     protected getQuery = (): string => `
         mutation updateMe($fields: UserUpdateInput!) {
             updateMe(fields: $fields) {
                 token
+                expiryDate
             }
         }
     `;
 }
 
-export class DeleteQuery extends Query{
+export class DeleteQuery extends Query {
     protected getQuery = (): string => `
         mutation deleteMe($password: String!) {
             deleteMe(password: $password)
         }
     `;
 }
+
+export class EmailAvailableQuery extends Query {
+    protected getQuery = (): string => `
+        query emailAvailable($email: String!) {
+            emailAvailable(email: $email)
+        }
+    `;
+}
+
+export class SendVerificationEmailQuery extends Query {
+    protected getQuery = (): string => `
+        query {
+            sendVerificationEmail
+        }
+    `;
+}
+
+export class PublicKeyQuery extends Query {
+    protected getQuery = (): string => `
+        query {
+            publicKey
+        }
+    `;
+}
+
+export class SendPasswordRecoveryQuery extends Query {
+    protected getQuery = (): string => `
+        query sendPasswordRecoveryEmail($email: String!) {
+            sendPasswordRecoveryEmail(email: $email)
+        }
+    `;
+}
+
+export class UserOneQuery extends QueryWithRequestedFields {
+
+    protected getQuery = (): string => `
+        query userOne($filter: FilterFindOneUserPublicInfoInput!) {
+            userOne(filter: $filter){
+                ...requestedFields
+            }
+        }  
+    `;
+}
+
+export class UserByIdsQuery extends QueryWithRequestedFields {
+
+    protected getQuery = (): string => `
+        query userByIds($ids: [MongoID]!) {
+            userByIds(_ids: $ids){
+                ...requestedFields
+            }
+        }
+    `;
+}
+
+export class UserManyQuery extends QueryWithRequestedFields {
+
+    protected getQuery = (): string => `
+        query userMany($filter: FilterFindManyUserPublicInfoInput!, limit: int) {
+            userMany(filter: $filter, limit: $limit){
+                ...requestedFields
+            }
+        }
+    `;
+}
+
+export class UserCountQuery extends Query {
+    protected getQuery = (): string => {
+        if(this.variables.filter){
+            return `
+                query userCount($filter: FilterUserPublicInfoInput!) {
+                    userCount(filter: $filter)
+                }
+            `
+        } else {
+            return `
+                query {
+                    userCount
+                }
+            `
+        }
+    }
+}
+
+export class UserPaginationQuery extends QueryWithRequestedFields {
+    protected getQuery = (): string => `
+        query userPagination($filter: FilterFindManyUserPublicInfoInput!, $page: int!, $perPage: int!) {
+            userPagination(filter: $filter, page: $page, perPage: $perPage){
+                ...requestedFields
+            }
+        }
+    `;
+}
+
