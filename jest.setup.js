264ae6b3f99761cac65c14dd10cfd776a9e2a86a
jrebecchi <johann.corcuff@gmail.com>
2020-05-02 14:04:51 +0200

    Increasing jest default time before failure

--- jest.setup.js
+++ jest.setup.js
@@ -5,3 +5,6 @@ window.Request = fetchPolifill.Request
 window.Headers = fetchPolifill.Headers
 window.Response = fetchPolifill.Response
 
+
+// Jest max time before failure
+jest.setTimeout(20000); // in milliseconds
